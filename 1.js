e61be1a9392d257dcb972c664dab9bfb6406ed73
pranjal96-12 <118056171+pranjal96-12@users.noreply.github.com>
2022-12-04 14:56:00 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,13 @@
+function handleclick(event){
+    const buttona =document.getElementById("button-a");
+    console.log(buttona);
+    
+    
+
+    
+}
+function ahandle(event){
+    const bub= document.getElementById("button-b");
+    // console.log(buttona);
+     console.log(bub);
+}
